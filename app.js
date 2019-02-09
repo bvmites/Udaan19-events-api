37598796d1276d07380e2a2039afb1d90b0ccb42
rushi7997 <ramirushi7997@gmail.com>
2019-02-09 15:43:45 +0530

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 
 const app = express();
 
@@ -19,7 +20,7 @@ const auth = require('./middleware/auth');
 dotenv.config();
 (async () => {
 
-    const client = await MongoClient.connect(process.env.DB);
+    const client = await MongoClient.connect(process.env.DB, { useNewUrlParser:true });
     const db = client.db('Udaan-19');
     const userDb = client.db('Udaan-19-users');
     console.log('Connected to database');
